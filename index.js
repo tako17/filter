4c611d437d9b9a6e11d91233feeb340a136699c2
tako17 <tako.chokoraia2000@gmail.com>
2022-04-06 01:34:12 +0400

    last three

---
+++ index.js
@@ -0,0 +1,11 @@
+$(".category").click(function(){
+    $("div.add").toggleClass("remove");
+  });
+
+  $(".color").click(function(){
+    $("div.add2").toggleClass("remove");
+  });
+
+  $(".Condition").click(function(){
+    $("div.add3").toggleClass("remove");
+  });
