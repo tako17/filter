5f6f3ae8ef7ad280f5580e7d39aab7a249ad8504
tako17 <tako.chokoraia2000@gmail.com>
2022-04-09 10:51:58 +0400

    change coler

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 $(".category").click(function(){
     $("div.add").toggleClass("remove");
+    $(".category").toggleClass("up");
+ 
   });
 
   $(".color").click(function(){
     $("div.add2").toggleClass("remove");
+    $(".color").toggleClass("up2");
+    
+    
   });
 
   $(".Condition").click(function(){
     $("div.add3").toggleClass("remove");
+    $(".Condition").toggleClass("up3");
   });
